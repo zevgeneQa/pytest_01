68df981145c81f8cd773f308bdae7ed747737f6b
zevgeneQa <106494505+zevgeneQa@users.noreply.github.com>
2025-06-30 16:01:21 +0300

    name

--- endpoints/create_object.py
+++ endpoints/create_object.py
@@ -6,7 +6,7 @@ class CreateObject(Endpoint):
 
 
     def new_object(self,payload):
-        self.response = requests.post(url='https://api.restful-api.dev/objects' ,json=payload)
+        self.response = requests.post(url='https://api.restful-api.dev/objects',json=payload)
         self.response_json = self.response.json()
 
     def check_name(self,name,payload):
--- pages/test_fe_ulkemp_auth.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test_login_api_dev():
-    payload = {"email" : "user@example.com","password" : "password"}
-    response=requests.post(url='https://ulcamp-backend-develop.k8s.zetest.site/api/login', json=payload).json()
-    print(response)
