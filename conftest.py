3a402494d9feb62e5895d4e0f4ff13b3ca988fde
zevgeneQa <106494505+zevgeneQa@users.noreply.github.com>
2025-07-16 14:07:02 +0300

    name

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ import pytest
 from endpoints.create_object import CreateObject
 from endpoints.delete_object import DeleteObject
 
-@pytest.fixture()
+@pytest.fixture() # Фикстура создает тестовый объект перед тестом и удаляет его после
 def obj_id():
-    create_object = CreateObject()
-    payload = {
+    create_object = CreateObject()  #  класс создания объекта
+    payload = { #Подготавливаем данные для создания объекта
         "name": "Apple MacBook Pro 16",
         "data": {
             "year": 2019,
@@ -14,12 +14,12 @@ def obj_id():
             "Hard disk size": "1 TB"
         }
     }
-    create_object.new_object(payload)
+    create_object.new_object(payload) #  запрос на создание объекта
    # response = requests.post(
    #     url='https://api.restful-api.dev/objects', json=payload
    # ).json()
-    yield create_object.response_json['id']
-    delete_object = DeleteObject()
+    yield create_object.response_json['id'] # возвращаем ID созданного объекта для использования в тестах
+    delete_object = DeleteObject() #  После выполнения теста!!! удаляем созданный объект
     delete_object.delete_by_id(create_object.response_json['id'])
     #requests.delete(f'https://api.restful-api.dev/objects/{response["id"]}')
 
--- test/test_api.py
+++ test/test_api.py
@@ -9,7 +9,7 @@ from endpoints.delete_object import DeleteObject
 
 
 
-def test_create_object():
+def test_create_object(): #  класс для работы с созданием объектов
     new_object_endpoint = CreateObject()
     payload = {
       "name": "Apple MacBook Pro 16",
@@ -20,9 +20,9 @@ def test_create_object():
          "Hard disk size": "1 TB"
       }
    }
-    new_object_endpoint.new_object(payload=payload)
+    new_object_endpoint.new_object(payload=payload) #  Отправляем запрос на создание объекта
     new_object_endpoint.check_response_is_200()
-    new_object_endpoint.check_name(payload)
+    new_object_endpoint.check_name(payload) #  проверка что имя в ответе соответствует отправленному
     #response = requests.post(
      #   url='https://api.restful-api.dev/objects' ,json=payload
    # ).json() #описанно в классе
@@ -30,11 +30,11 @@ def test_create_object():
     #print(response) #описанно в классе
 
 
-def test_get_object(obj_id):
+def test_get_object(obj_id): # Получаем ID объекта из фикстуры
     get_obj_endpoint = GetObject()
-    get_obj_endpoint.get_by_id(obj_id)
+    get_obj_endpoint.get_by_id(obj_id) # запрос на получение объекта по ID
     get_obj_endpoint.check_response_is_200()
-    get_obj_endpoint.check_response_id(obj_id)
+    get_obj_endpoint.check_response_id(obj_id) # проверка что полученный объект содержит ожидаемые данные
 
     #print(obj_id)
     #response=requests.get(f'https://api.restful-api.dev/objects/{obj_id}').json()
@@ -64,13 +64,13 @@ def test_update_object(obj_id):
     #print(response)
 
 
-def test_delete_object(obj_id, get_obj_endpoint=None):
-    get_obj_endpoint = GetObject()
-    delete_obj_endpoint = DeleteObject()
-    delete_obj_endpoint.delete_by_id(obj_id)
-    delete_obj_endpoint.check_response_is_200()
-    get_obj_endpoint.get_by_id(obj_id)
-    get_obj_endpoint.check_response_is_404()
+def test_delete_object(obj_id): # Получаем ID объекта из фикстуры
+    get_obj_endpoint = GetObject() #  пытаемся получить удаленный объект
+    delete_obj_endpoint = DeleteObject() #  Инициализируем класс для работы с удалением объектов
+    delete_obj_endpoint.delete_by_id(obj_id)  #   запрос на удаление объекта по ID
+    delete_obj_endpoint.check_response_is_200() #  Проверяем  статус ответа 200 (OK)
+    get_obj_endpoint.get_by_id(obj_id) #  пытаемся получить удаленный объект
+    get_obj_endpoint.check_response_is_404() # объект действительно удален (статус 404)
     #response = requests.delete(f'https://api.restful-api.dev/objects/{obj_id}')
     #assert response.status_code == 200
     #response = requests.get(f'https://api.restful-api.dev/objects/{obj_id}')
